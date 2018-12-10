72985cd36689a48b7a62cd3f37fea463e0507479
Clément Baconnier <clement.baconnier@gmail.com>
2018-12-10 13:41:11 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,21 @@
+# Introduction
+
+Disable the current version of PHP and enable your desired PHP version.
+I'm using this for my own needs. It won't necessary fit yours.
+
+# Installation
+
+    $ cp switchphp.sh /etc/bin/switchphp
+    $ chmod +x /etc/bin/switchphp
+
+# Usage
+Switch to php 7.3 (pre-installed)
+
+    $ switchphp 7.3
+    
+# What does it do?
+- Disable the current PHP version
+- Stop the PHP FPM service
+- Enable your desired PHP version
+- Start the PHP FPM service 
+- Update alternatives
---
+++ switchphp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+current=`php -r "echo phpversion();" | cut -f1,2 -d'.'`
+choice=$1
+
+
+if [ $# -lt 1 ]
+then
+  echo "Usage: $0 PHPversion"
+  exit 1
+fi
+
+
+echo "Disabling current version: $current"
+sudo a2dismod "php$current" >> /dev/null
+sudo service "php$current-fpm" stop
+
+echo "Enabling version: $current"
+sudo a2enmod "php$choice"  >> /dev/null
+sudo service "php$choice-fpm" start
+
+echo "switching to version $choice"
+sudo update-alternatives --set php "/usr/bin/php$choice" >> /dev/null
+
+echo "All done!"
